f7fd99df7b7107efd856226c3a25c1fab2951056
princegobah <babynamek@gmail.com>
2023-09-20 21:02:54 -0400

    sensor

--- road.js
+++ road.js
@@ -21,7 +21,7 @@ class Road{
         ];
     }
 
-    getLaneCenter(laneIndex){h
+    getLaneCenter(laneIndex){
         const laneWidth=this.width/this.laneCount;
         return this.left+laneWidth/2+
             Math.min(laneIndex,this.laneCount-1)*laneWidth;
